9ccce2c08df78eba4aa5b32414eed6c50930dd17
rahular99 <rahul.ar@talrop.com>
2025-12-29 08:56:56 +0530

    destination model created

--- user_destination/admin.py
+++ user_destination/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import user_journey_record
+
+
+
+@admin.register(user_journey_record)
+class userJourneyRecord(admin.ModelAdmin):
+    list_display =(
+        'id',
+        'user',
+        'destination_from',
+        'destination_to'
+    )
--- user_destination/models.py
+++ user_destination/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class user_journey_record(models.Model):
+    user = models.CharField(max_length=255)
+    destination_from = models.CharField(max_length=255)
+    destination_to = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    budget = models.IntegerField(max_length=10)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
